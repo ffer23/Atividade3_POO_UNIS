e487bdc39ed930f94cea62b4fffa72376fc02284
Fernanda F. Fonseca <65093157+ffer23@users.noreply.github.com>
2020-05-25 19:16:47 -0300

    Add files via upload

---
+++ CalculoImc.java
@@ -0,0 +1,46 @@
+package calculoimc;
+
+import javax.swing.JOptionPane;
+
+public class CalculoImc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float peso, altura, calcImc;
+        String respPeso, respAltura;
+        
+        respPeso = JOptionPane.showInputDialog("Digite o seu peso em kg: ");
+        peso = Integer.parseInt(respPeso);
+        
+        respAltura = JOptionPane.showInputDialog("Digite a sua altura em cm: ");
+        altura = Integer.parseInt(respAltura);
+        
+        calcImc = peso / ((altura / 100) * (altura / 100));
+        
+        if (calcImc < 17){
+            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
+                    "\t Situação: Muito abaixo do peso");            
+        }else if ((calcImc > 17) && (calcImc < 18.49)){
+            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
+                    "\t Situação: Abaixo do peso");        
+        } else if ((calcImc > 18.50) && (calcImc < 24.99)){
+            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
+                    "\t Situação: Peso normal"); 
+        } else if ((calcImc > 25) && (calcImc < 29.99)){
+            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
+                    "\t Situação: Acima do peso");
+        }else if ((calcImc > 30) && (calcImc < 34.99)){
+            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
+                    "\t Situação: Obesidade I");
+        }else if ((calcImc > 35) && (calcImc < 39.99)){
+            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
+                    "\t Situação: Obesidade II [severa]");
+        }else{
+            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
+                    "\t Situação: Obesidade III [mórbida]");
+        }               
+    }
+}
