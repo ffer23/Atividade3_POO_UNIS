36f8e86eb9f61026ed91b92a3e16774b57743eb4
Fernanda F. Fonseca <65093157+ffer23@users.noreply.github.com>
2020-05-26 09:46:09 -0300

    Update CalculoImc.java

--- CalculoImc.java
+++ CalculoImc.java
@@ -21,26 +21,26 @@ public class CalculoImc {
         calcImc = peso / ((altura / 100) * (altura / 100));
         
         if (calcImc < 17){
-            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
-                    "\t Situação: Muito abaixo do peso");            
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+                    calcImc) + "\n Situação: Muito abaixo do peso");            
         }else if ((calcImc > 17) && (calcImc < 18.49)){
-            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
-                    "\t Situação: Abaixo do peso");        
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+                    calcImc) + "\n Situação: Abaixo do peso");        
         } else if ((calcImc > 18.50) && (calcImc < 24.99)){
-            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
-                    "\t Situação: Peso normal"); 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+                    calcImc) + "\n Situação: Peso normal"); 
         } else if ((calcImc > 25) && (calcImc < 29.99)){
-            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
-                    "\t Situação: Acima do peso");
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+                    calcImc) + "\n Situação: Acima do peso");
         }else if ((calcImc > 30) && (calcImc < 34.99)){
-            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
-                    "\t Situação: Obesidade I");
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+                    calcImc) + "\n Situação: Obesidade I");
         }else if ((calcImc > 35) && (calcImc < 39.99)){
-            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
-                    "\t Situação: Obesidade II [severa]");
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+                    calcImc) + "\n Situação: Obesidade II [severa]");
         }else{
-            JOptionPane.showMessageDialog(null, "O seu IMC foi: " + calcImc +
-                    "\t Situação: Obesidade III [mórbida]");
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+                    calcImc) + "\n Situação: Obesidade III [mórbida]");
         }               
     }
 }
