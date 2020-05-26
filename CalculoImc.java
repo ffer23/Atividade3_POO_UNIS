4b617e5153f72bae4ff7723374b658f0ff450974
Fernanda F. Fonseca <65093157+ffer23@users.noreply.github.com>
2020-05-26 18:43:18 -0300

    Update CalculoImc.java

--- CalculoImc.java
+++ CalculoImc.java
@@ -21,25 +21,25 @@ public class CalculoImc {
         calcImc = peso / ((altura / 100) * (altura / 100));
         
         if (calcImc < 17){
-            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC é: %.2f", 
                     calcImc) + "\n Situação: Muito abaixo do peso");            
         }else if ((calcImc > 17) && (calcImc < 18.49)){
-            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC é: %.2f", 
                     calcImc) + "\n Situação: Abaixo do peso");        
         }else if ((calcImc > 18.50) && (calcImc < 24.99)){
-            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC é: %.2f", 
                     calcImc) + "\n Situação: Peso normal"); 
         }else if ((calcImc > 25) && (calcImc < 29.99)){
-            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC é: %.2f", 
                     calcImc) + "\n Situação: Acima do peso");
         }else if ((calcImc > 30) && (calcImc < 34.99)){
-            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC é: %.2f", 
                     calcImc) + "\n Situação: Obesidade I");
         }else if ((calcImc > 35) && (calcImc < 39.99)){
-            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC é: %.2f", 
                     calcImc) + "\n Situação: Obesidade II [severa]");
         }else{
-            JOptionPane.showMessageDialog(null, String.format("O seu IMC foi: %.2f", 
+            JOptionPane.showMessageDialog(null, String.format("O seu IMC é: %.2f", 
                     calcImc) + "\n Situação: Obesidade III [mórbida]");
         }               
     }
